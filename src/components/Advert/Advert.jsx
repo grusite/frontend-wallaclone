06c75d97a8fd0e77dacc9ba8fc4317092f8da897
Jorge Martín <grusite@gmail.com>
2020-02-22 20:01:21 +0100

    added Heroku url in advert comp to download imgs

--- src/components/Advert/Advert.jsx
+++ src/components/Advert/Advert.jsx
@@ -1,55 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined';
-import AttachMoneyOutlinedIcon from '@material-ui/icons/AttachMoneyOutlined';
-import Grid from '@material-ui/core/Grid';
-import EditIcon from '@material-ui/icons/Edit';
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined'
+import AttachMoneyOutlinedIcon from '@material-ui/icons/AttachMoneyOutlined'
+import Grid from '@material-ui/core/Grid'
+import EditIcon from '@material-ui/icons/Edit'
 
-import './advert.css';
+import './advert.css'
 
 export default function Advert({ advert, history }) {
-  const [imgUrl, setimgUrl] = useState(advert.picture);
-  const formattedDate = new Date(advert.createdAt).toDateString();
+  const [imgUrl, setimgUrl] = useState(advert.picture)
+  const formattedDate = new Date(advert.createdAt).toDateString()
 
   if (imgUrl.startsWith('/images/adverts'))
-    setimgUrl(`http://localhost:8080${advert.picture}`);
+    setimgUrl(`https://ancient-depths-90365.herokuapp.com/${advert.picture}`)
 
   const goToDetail = () => {
-    history.push(`/advert/${advert._id}`);
-  };
+    history.push(`/advert/${advert._id}`)
+  }
 
   const goToUpdate = () => {
-    history.push(`/update/${advert._id}`);
-  };
+    history.push(`/update/${advert._id}`)
+  }
 
   const avatar = type => {
     if (type === 'buy') {
       return (
-        <Avatar
-          id="avatar-green-no-material"
-          aria-label="adv"
-          className="avatar"
-        >
+        <Avatar id="avatar-green-no-material" aria-label="adv" className="avatar">
           <ShoppingBasketOutlinedIcon />
         </Avatar>
-      );
+      )
     } else if (type === 'sell') {
       return (
         <Avatar id="avatar-no-material" aria-label="adv" className="avatar">
           <AttachMoneyOutlinedIcon />
         </Avatar>
-      );
+      )
     }
-  };
+  }
 
   return (
     <>
@@ -65,19 +61,9 @@ export default function Advert({ advert, history }) {
             }
             subheader={formattedDate}
           />
-          <CardMedia
-            className="media"
-            image={imgUrl}
-            title={advert.name}
-            onClick={goToDetail}
-          />
+          <CardMedia className="media" image={imgUrl} title={advert.name} onClick={goToDetail} />
           <CardContent onClick={goToDetail}>
-            <Typography
-              id="card-description"
-              variant="body2"
-              color="textSecondary"
-              component="p"
-            >
+            <Typography id="card-description" variant="body2" color="textSecondary" component="p">
               {advert.description}
             </Typography>
             <Typography id="card-price" variant="h5" component="p">
@@ -100,5 +86,5 @@ export default function Advert({ advert, history }) {
         </Card>
       </Grid>
     </>
-  );
+  )
 }
